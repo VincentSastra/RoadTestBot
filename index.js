03b4615cda338684d51f6c075624dab72a07d0ee
VincentSastra <vincent.sastra@gmail.com>
2021-07-12 01:03:58 -0700

    Make it an express app or heroku kills it

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ checkAppointment();
 setInterval(checkAppointment, 1000 * 60 * 5);
 sendUptime();
 setInterval(sendUptime, 1000 * 60 * 60 * 24);
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Status OK! Checking for appointments before ' + new Date(process.env.searchdate).toDateString())
+})
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Listening to http://localhost:${port}`)
+})
