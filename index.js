13583eab3f5485d394102bb8366539a42088313d
VincentSastra <vincent.sastra@gmail.com>
2021-07-15 18:21:31 -0700

    Change to app to be a Lambda function instead

--- index.js
+++ index.js
@@ -1,23 +1,86 @@
 const fetch = require('node-fetch')
 const nodemailer = require('nodemailer')
-const { checkAppointment } = require('./checkAppointment');
-const { sendUptime } = require('./sendUptime');
 
 require('dotenv').config()
 
-checkAppointment();
-setInterval(checkAppointment, 1000 * 60 * 5);
-sendUptime();
-setInterval(sendUptime, 1000 * 60 * 60 * 24);
+exports.handler =  async (event, context) => {
+	const mail = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: process.env.gmailname,
+			pass: process.env.gmailpass,
+		}
+	});
+	
+	const searchDate = new Date(process.env.searchdate)
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000;
+	try {
+		let body = JSON.stringify({
+			drvrLastName: process.env.lastname,
+			keyword: process.env.keyword,
+			licenceNumber: process.env.license,
+		})
 
-app.get('/', (req, res) => {
-  res.send('Status OK! Checking for appointments before ' + new Date(process.env.searchdate).toDateString())
-})
+		const authReq = await fetch('https://onlinebusiness.icbc.com/deas-api/v1/webLogin/webLogin', {
+			method: 'put',
+			body: body,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+		console.log(authReq)
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Listening to http://localhost:${port}`)
-})
+		const authToken = authReq.headers.raw().authorization[0]
+
+		const appointmentRequest = await fetch('https://onlinebusiness.icbc.com/deas-api/v1/web/getAvailableAppointments', {
+			method: 'post',
+			body: JSON.stringify({
+				aPosID: 8,
+				examDate: "2021-07-12",
+				examType: "5-R-1",
+				ignoreReserveTime: false,
+				lastName: process.env.lastname,
+				licenseNumber: process.env.license,
+				prfDaysOfWeek: "[0,1,2,3,4,5,6]",
+				prfPartsOfDay: "[0,1]",
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': authToken,
+			}
+		})
+
+		const appointmentJson = await appointmentRequest.json()
+
+		const availableBefore = appointmentJson.find(row => {
+			const appointmentDate = new Date(row.appointmentDt.date)
+			return appointmentDate.getTime() < searchDate.getTime()
+		})?.appointmentDt?.date
+		
+		console.log("All date: \n" +
+			appointmentJson.reduce((acc, cur) => {
+				const appointmentDate = new Date(cur.appointmentDt.date)
+				return acc + "\n" + appointmentDate 
+			}, ""))
+
+		if (availableBefore !== undefined) {
+			mail.sendMail({
+				from: process.env.gmailname,
+				to: process.env.gmailgoal,
+				subject: 'Booking found on ' + new Date(availableBefore).toDateString(),
+				text: 'Gogogo!',
+			})
+			console.log("Found" + availableBefore)
+		} else {
+			console.log("Earliest is " + (new Date(appointmentJson[0]?.appointmentDt?.date)))
+		}
+	} catch (e) {
+		console.log(e)
+		mail.sendMail({
+			from: process.env.gmailname,
+			to: process.env.gmailgoal,
+			subject: 'Error',
+			text: JSON.stringify(e),
+		})
+	}
+}
